e8de77c30eb869acd14f47466982da4b3af65c4d
Jan Wilczak <janwilczak@gmail.com>
2018-12-14 18:00:06 +0000

    init commit

---
+++ DevOpsTest/DevOpsTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevOpsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
