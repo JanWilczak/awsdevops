baabbd9cf1e1d0a035639253aaff09206d6bd614
Jan Wilczak <janwilczak@gmail.com>
2018-12-17 14:10:16 +0000

    aaa

--- aws-DevOps-Test/Controllers/ValuesController.cs
+++ aws-DevOps-Test/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace aws_DevOps_Test.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "jan-test-tutaj", "mAster" };
+            return new string[] { "jan-test-tutaj", "mAsteraaaa" };
         }
 
         // GET api/values/5
